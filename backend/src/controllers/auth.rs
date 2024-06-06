a1b0bf220914383ff64c731d0fe9786f3b65ef14
zam-cv <a01799283@tec.mx>
2024-06-06 12:51:38 -0600

    add: signin

--- backend/src/controllers/auth.rs
+++ backend/src/controllers/auth.rs
@@ -1,23 +1,34 @@
-use crate::{database::Database, models, utils};
+use crate::{database::Database, models, utils, config};
 use actix_web::{post, web, HttpResponse, Responder};
+use serde::{Deserialize, Serialize};
 use validator::Validate;
+
+#[derive(Deserialize, Serialize)]
+pub struct Credentials {
+    pub email: String,
+    pub password: String,
+}
 
 #[post("/register")]
 async fn register(
     mut user: web::Json<models::User>,
     database: web::Data<Database>,
 ) -> impl Responder {
+    // Validate the user data
     if let Err(_) = user.validate() {
         return HttpResponse::BadRequest().body("Invalid data");
     }
 
+    // Check if the email already exists
     if let Ok(Some(_)) = database.get_user_by_email(user.email.clone()).await {
         return HttpResponse::BadRequest().body("Email already exists");
     }
 
+    // Hash the password
     if let Ok(hash) = utils::hash_password(&user.password) {
         user.password = hash;
 
+        // Create the user
         return match database.create_user(user.into_inner()).await {
             Ok(_) => HttpResponse::Ok().body("User created"),
             Err(_) => HttpResponse::InternalServerError().body("Failed to create user"),
@@ -28,8 +39,27 @@ async fn register(
 }
 
 #[post("/signin")]
-async fn signin() -> impl Responder {
-    HttpResponse::Ok().body("Signin")
+async fn signin(
+    credentials: web::Json<Credentials>,
+    database: web::Data<Database>,
+) -> impl Responder {
+    // Get the user by email
+    let user = match database.get_user_by_email(credentials.email.clone()).await {
+        Ok(Some(user)) => user,
+        _ => return HttpResponse::BadRequest().body("Invalid credentials"),
+    };
+
+    // Verify the password
+    if let Ok(true) = utils::verify_password(&credentials.password, &user.password) {
+        if let Some(id) = user.id {
+            // Create a token
+            if let Ok(token) = utils::create_token(&config::SECRET_KEY, id) {
+                return HttpResponse::Ok().body(token);
+            }
+        }
+    };
+
+    HttpResponse::BadRequest().body("Invalid credentials")
 }
 
 #[post("/verify")]
