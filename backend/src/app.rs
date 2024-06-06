221610fd324e6812fba0d8caef69c883a3c85a1f
zam-cv <a01799283@tec.mx>
2024-06-05 23:14:56 -0600

    add: app

---
+++ backend/src/app.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, middleware::Logger, App, HttpServer};
+use std::env;
+
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello, world!"
+}
+
+pub async fn app() -> std::io::Result<()> {
+    HttpServer::new(move || App::new().wrap(Logger::default()).service(index))
+        // Use the HOST and PORT environment variables to bind the server
+        .bind(format!(
+            "{}:{}",
+            env::var("HOST").unwrap_or("0.0.0.0".to_string()),
+            env::var("PORT").unwrap_or("8080".to_string())
+        ))?
+        .run()
+        .await
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+
+mod app;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Load the environment variables
+    dotenv().ok();
+
+    // Initialize the logger
+    env_logger::init();
+
+    // Start the application
+    app::app().await
 }
