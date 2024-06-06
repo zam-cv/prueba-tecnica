ffdc9629d1a44f833ae4140e5db788fdafb699fc
zam-cv <a01799283@tec.mx>
2024-06-06 16:09:47 -0600

    add-back: cors

--- backend/src/app.rs
+++ backend/src/app.rs
@@ -1,4 +1,5 @@
 use crate::{controllers, database::Database};
+use actix_cors::Cors;
 use actix_web::{middleware::Logger, web, App, HttpServer};
 use std::env;
 
@@ -8,8 +9,12 @@ pub async fn app() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
+            // Enable the logger and CORS middleware
+            .wrap(Cors::permissive())
             .wrap(Logger::default())
+            // Resources
             .app_data(web::Data::new(db.clone()))
+            // Routes
             .service(web::scope("/api").service(controllers::auth::routes()))
     })
     // Use the HOST and PORT environment variables to bind the server
